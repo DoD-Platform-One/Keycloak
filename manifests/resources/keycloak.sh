058a6bccd7da09f468de688bba9558453adc24ed
Jeff McCoy <me@jeffm.us>
2020-06-29 16:57:49 -0600

    cleanup the prod deployment failures

--- manifests/resources/keycloak.sh
+++ manifests/resources/keycloak.sh
@@ -3,4 +3,5 @@
 set -o errexit
 set -o nounset
 
-exec /opt/jboss/tools/docker-entrypoint.sh -b 127.0.0.1 -c standalone-ha.xml
+# SAD! @todo can we bind to a real IP here?  127.0.0.1 only works with Istio, but hard-breaks in prod for client token retrieval
+exec /opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0 -c standalone-ha.xml
